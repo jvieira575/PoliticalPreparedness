a6a67f575b25dc69663ca24b43296745f5683170
John Vieira <jvieira575@gmail.com>
2021-03-04 19:24:45 -0600

    chore: Add snackbar when error is encountered retrieving upcoming elections.

--- app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/ElectionsFragment.kt
@@ -8,11 +8,14 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.example.android.politicalpreparedness.R
 import com.example.android.politicalpreparedness.databinding.FragmentElectionBinding
 import com.example.android.politicalpreparedness.election.adapter.ElectionListAdapter
 import com.example.android.politicalpreparedness.election.adapter.ElectionListener
+import com.example.android.politicalpreparedness.network.domain.ApiStatus
 import com.example.android.politicalpreparedness.network.models.Division
 import com.example.android.politicalpreparedness.network.models.Election
+import com.google.android.material.snackbar.Snackbar
 import timber.log.Timber
 
 
@@ -63,6 +66,15 @@ class ElectionsFragment: Fragment() {
         viewModel.savedElections.observe(viewLifecycleOwner, Observer<List<Election>> { savedElections ->
             savedElections?.apply {
                 savedElectionListAdapter.elections = savedElections
+            }
+        })
+
+        viewModel.status.observe(viewLifecycleOwner, Observer<ApiStatus> { apiStatus ->
+            when(apiStatus) {
+                ApiStatus.ERROR -> {
+                    Snackbar.make(requireView(), R.string.error_upcoming_elections, Snackbar.LENGTH_LONG).show()
+                }
+                else -> {}
             }
         })
 
